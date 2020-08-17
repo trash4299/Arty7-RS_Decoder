14d08d471691b84eff4567b9bcd49fa5b96991a0
Vincent Lobo <vincelobo4299@gmail.com>
2020-08-17 11:31:05 -0500

    Update decoderAXIStreamWrapper.v

--- RSdecoder/src/hdl/decoderAXIStreamWrapper.v
+++ RSdecoder/src/hdl/decoderAXIStreamWrapper.v
@@ -31,9 +31,9 @@ module decoderAXIStreamWrapper (
 	
 	
 	//  additional ports here
-	output reg vld_in,
-	output reg [104:0] dta_in,
-	output reg [24:0] synd_in,
+	output reg vld_in = 0,
+	output reg [104:0] dta_in = 105'd0,
+	output reg [24:0] synd_in=25'd0,
 	//output reg [7:0] k_in,
 	//output reg [15:0] rssi_in,
 	//output bad_en,
@@ -45,25 +45,10 @@ module decoderAXIStreamWrapper (
 	input [2:0] pkt_errors
 );
 	
-	//assign bad_en = 0;
-	reg pkt_vldreg = 0;
-	reg [104:0] pkt_dtareg = 105'b0;
-	reg pkt_newreg = 0;
-	reg [2:0] pkt_errorsreg = 3'b0;
-	
-	always @(posedge m_axis_aclk) begin
-		if(pkt_vld == 1) begin
-			pkt_vldreg <= pkt_vld;
-			pkt_dtareg <= pkt_dta;
-			pkt_newreg <= pkt_new;
-			pkt_errorsreg <= pkt_errors;
-		end
-	end
-	
-	reg [1:0] wr_ptr_reg = 2'b0, wr_ptr_next;
-	reg [1:0] wr_addr_reg = 2'b0;
-	reg [1:0] rd_ptr_reg = 2'b0, rd_ptr_next;
-	reg [1:0] rd_addr_reg = 2'b0;
+	reg [2:0] wr_ptr_reg = 3'd3, wr_ptr_next;
+	reg [2:0] wr_addr_reg = 3'd3;
+	reg [2:0] rd_ptr_reg = 3'd3, rd_ptr_next;
+	reg [2:0] rd_addr_reg = 3'd3;
 
 	reg s_rst_sync1_reg = 1'b1;
 	reg s_rst_sync2_reg = 1'b1;
@@ -71,8 +56,6 @@ module decoderAXIStreamWrapper (
 	reg m_rst_sync1_reg = 1'b1;
 	reg m_rst_sync2_reg = 1'b1;
 	reg m_rst_sync3_reg = 1'b1;
-	
-	// reg [64+2-1:0] mem[(2**32)-1:0];
 	reg [63:0] memIN[3:0];
 	reg [63:0] memOUT[3:0];
 	reg [63:0] mem_read_data_reg = 64'b0;
@@ -81,33 +64,52 @@ module decoderAXIStreamWrapper (
 
 	reg [63:0] m_data_reg = 64'b0;
 
-	reg m_axis_tvalid_reg = 1'b0, m_axis_tvalid_next;
+	reg m_axis_tvalid_reg = 1'b0;
+	wire m_axis_tvalid_next;
+	reg vld_inreg;
 	
 	always@(*) begin
-		vld_in <= memIN[3][0:0];
-		dta_in[104:0] <= {memIN[0][63:0],memIN[1][40:0]};
-		synd_in[24:0] <= memIN[2][24:0];
-//		k_in[7:0] <=  memIN[2][32:25];
-//		rssi_in [15:0]  <=  memIN[2][48:33];
-//		bad_en <=  memIN[3][49:49];
-		pkt_vldreg <=  memOUT[3][0:0];
-		pkt_dtareg[104:0]  <= {memOUT[0][63:0],memOUT[1][40:0]};
-//		pkt_rssi[15:0]  <= memOUT[2][15:0];
-//		pkt_k[7:0] <= memOUT[2][23:16];
-		pkt_newreg <= memOUT[3][1:1];
-		pkt_errorsreg[2:0]  <= memOUT[3][4:2];
+		vld_inreg <= memIN[3][32:32];
+		dta_in[104:0] <= {memIN[2][36:32],memIN[2][28:24],memIN[2][20:16],memIN[2][12:8],memIN[2][4:0],memIN[1][60:56],memIN[1][52:48],
+			memIN[1][44:40],memIN[1][36:32],memIN[1][28:24],memIN[1][20:16],memIN[1][12:8],memIN[1][4:0],memIN[0][60:56],memIN[0][52:48],
+			memIN[0][44:40],memIN[0][36:32],memIN[0][28:24],memIN[0][20:16],memIN[0][12:8],memIN[0][4:0]};
+		synd_in[24:0] <= {memIN[3][12:8],memIN[3][4:0],memIN[2][60:56],memIN[2][52:48],memIN[2][44:40]};
 	end
-
+	
+	always@(posedge s_axis_aclk) begin
+		if(vld_inreg) begin
+			vld_in <= 1;
+		end
+		if(vld_in == 1) begin
+			vld_in <= 0;
+			vld_inreg <= 0;
+			wr_ptr_reg <= 3'd0;
+		end
+	end
+	
+	reg pkt_vldreg = 0;
+	always @(posedge m_axis_aclk) begin
+		if(pkt_vld == 1) begin
+			pkt_vldreg <= pkt_vld;
+			{memOUT[0][63:0],memOUT[1][40:0]} <= pkt_dta;
+			memOUT[3][1:1] <= pkt_new;
+			memOUT[3][4:2] <= pkt_errors;
+			rd_ptr_reg <= 3'd0;
+		end
+	end
+	
+	
 	// full when ptr hits 3
-	wire full = (wr_ptr_reg == 2'd3);
+	wire full = (wr_ptr_reg == 3'd4);
 	// empty when ptr hits 3
-	wire empty = (rd_ptr_reg == 2'd3);
-
+	wire empty = (rd_ptr_reg == 3'd4);
+	
 	// control signals
 	reg write, read, store_output;
 
-	assign s_axis_tready = ~full & ~s_rst_sync3_reg;
-
+	assign s_axis_tready = (~full & ~s_rst_sync3_reg);
+	assign m_axis_tvalid_next = (~empty & ~m_rst_sync3_reg);
+	
 	assign m_axis_tvalid = m_axis_tvalid_reg;
 
 	assign mem_write_data = s_axis_tdata;
@@ -141,22 +143,20 @@ module decoderAXIStreamWrapper (
 	// Write logic   MM2Dec
 	always @* begin
 		write = 1'b0;
-		
+		wr_ptr_next = wr_ptr_reg;
 		if (s_axis_tvalid) begin
 			// input data valid
-			if (~full) begin			//I DONT THINK IT SHOULD NEED THIS BECAUSE DMA WILL ONLY SEND 4 PACKETS
+			if (~full) begin
 				// not full, perform write
 				write = 1'b1;
 				wr_ptr_next = wr_ptr_reg + 1'b1;
 			end
-			else
-				wr_ptr_next = 2'b0;
 		end
 	end
 
 	always @(posedge s_axis_aclk) begin
 		if (s_rst_sync3_reg) begin
-			wr_ptr_reg <= 2'b0;
+			wr_ptr_reg <= 3'b0;
 		end else begin
 			wr_ptr_reg <= wr_ptr_next;
 		end
@@ -164,30 +164,9 @@ module decoderAXIStreamWrapper (
 		wr_addr_reg <= wr_ptr_next;
 
 		if (write) begin
-			memIN[wr_addr_reg[1:0]] <= mem_write_data;
+			memIN[wr_addr_reg[2:0]] <= mem_write_data;
 		end
 	end
-
-	// pointer synchronization
-	// always @(posedge s_axis_aclk) begin
-		// if (s_rst_sync3_reg) begin
-			// rd_ptr_gray_sync1_reg <= {32+1{1'b0}};
-			// rd_ptr_gray_sync2_reg <= {32+1{1'b0}};
-		// end else begin
-			// rd_ptr_gray_sync1_reg <= rd_ptr_gray_reg;
-			// rd_ptr_gray_sync2_reg <= rd_ptr_gray_sync1_reg;
-		// end
-	// end
-
-	// always @(posedge m_axis_aclk) begin
-		// if (m_rst_sync3_reg) begin
-			// wr_ptr_gray_sync1_reg <= {32+1{1'b0}};
-			// wr_ptr_gray_sync2_reg <= {32+1{1'b0}};
-		// end else begin
-			// wr_ptr_gray_sync1_reg <= wr_ptr_gray_reg;
-			// wr_ptr_gray_sync2_reg <= wr_ptr_gray_sync1_reg;
-		// end
-	// end
 
 	// Read logic    Dec2MM
 	always @* begin
@@ -199,22 +178,26 @@ module decoderAXIStreamWrapper (
 
 		if (store_output | ~mem_read_data_valid_reg) begin
 			// output data not valid OR currently being transferred
-			if (~empty&&memOUT[3][0]) begin
+			if (~empty&pkt_vldreg) begin
 				// not empty, perform read and new packet avaliable
 				read = 1'b1;
 				mem_read_data_valid_next = 1'b1;
 				rd_ptr_next = rd_ptr_reg + 1;
 			end else begin
 				// empty, invalidate
-				mem_read_data_valid_next = 1'b0;
+				if(pkt_vldreg) begin
+					mem_read_data_valid_next = 1'b0;
+					pkt_vldreg <= 0;
+				end
 			end
 		end
 	end
 
 	always @(posedge m_axis_aclk) begin
 		if (m_rst_sync3_reg) begin
-			rd_ptr_reg <= 2'b0;
-			mem_read_data_valid_reg <= 1'b0;
+			rd_ptr_reg <= 3'b0;
+			mem_read_data_valid_reg <= 0;
+			pkt_vldreg <= 0;
 		end else begin
 			rd_ptr_reg <= rd_ptr_next;
 			mem_read_data_valid_reg <= mem_read_data_valid_next;
@@ -223,7 +206,7 @@ module decoderAXIStreamWrapper (
 		rd_addr_reg <= rd_ptr_next;
 
 		if (read) begin
-			mem_read_data_reg <= memOUT[rd_addr_reg[1:0]];
+			mem_read_data_reg <= memOUT[rd_addr_reg[2:0]];
 		end
 	end
 
@@ -231,11 +214,8 @@ module decoderAXIStreamWrapper (
 	always @* begin
 		store_output = 1'b0;
 
-		m_axis_tvalid_next = m_axis_tvalid_reg;
-
 		if (m_axis_tready | ~m_axis_tvalid) begin
 			store_output = 1'b1;
-			m_axis_tvalid_next = mem_read_data_valid_reg;
 		end
 	end
 
